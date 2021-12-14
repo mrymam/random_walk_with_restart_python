fea43abfc3b679d8e232635e175ba1d7e5eacc9c
onyanko-pon <engineer.tsukasa@gmail.com>
2021-12-14 12:50:59 +0900

    setup

---
+++ random_walk_with_restart/__init__.py
@@ -0,0 +1,10 @@
+from .rwr import *
+
+__copyright__    = 'Copyright (C) 2021 Tsukasa Maruyama'
+__version__      = '0.0.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'onyankopon'
+__author_email__ = 'engineer.tsukasa@gmail.com'
+__url__          = 'http://github.com/onyanko-pon/random_walk_with_restart_python'
+
+__all__ = [RWRGraph]
